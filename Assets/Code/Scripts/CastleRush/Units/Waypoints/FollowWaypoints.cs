657127f9b293777c19315010d1846f9dbbf57987
Zhm17 <zhemo17@gmail.com>
2024-07-25 02:50:07 -0600

    - Rotate towards target

--- Assets/Code/Scripts/CastleRush/Units/Waypoints/FollowWaypoints.cs
+++ Assets/Code/Scripts/CastleRush/Units/Waypoints/FollowWaypoints.cs
@@ -1,20 +1,33 @@
+using CastleRush.Units;
 using System.Collections;
 using UnityEngine;
 
 namespace CasteRush.Units
 {
+    [RequireComponent(typeof(RotateTowardsTarget))]
     public class FollowWaypoints : MonoBehaviour
     {
         [SerializeField] private Transform m_currentTarget;
+        private Transform CurrentTarget => m_currentTarget;
+        private void SetCurrentTarget(Transform transform)
+        {
+            m_currentTarget = transform;
+            if(TryGetComponent<RotateTowardsTarget>(out RotateTowardsTarget rtt))
+            {
+                rtt.SetTarget(CurrentTarget);
+            }
+        }
+
+
         [SerializeField] private int m_currentWaypointIndex = 0;
 
-        [SerializeField] private float m_startSpeed = 10f;
+        [SerializeField] private float m_startSpeed = 5f;
         [SerializeField] private float m_walkSpeed = 5f;
 
         // Start is called before the first frame update
         public void StartWalking()
         {
-            m_currentTarget = PathWaypoints.Points[0];
+            SetCurrentTarget( PathWaypoints.Points[0]);
             
             if(m_currentWaypointIndex > 0 ) 
                 Reset();
@@ -31,10 +44,10 @@ namespace CasteRush.Units
         {
             while(true)
             {
-                Vector3 dir = m_currentTarget.position - transform.position;
-                transform.Translate(dir.normalized * m_walkSpeed * Time.deltaTime, Space.World);
+                Vector3 direction = CurrentTarget.position - transform.position;
+                transform.Translate(direction.normalized * m_walkSpeed * Time.deltaTime, Space.World);
 
-                if (Vector3.Distance(transform.position, m_currentTarget.position) <= 0.4f)
+                if (Vector3.Distance(transform.position, CurrentTarget.position) <= 0.4f)
                 {
                     GetNextWaypoint();
                 }
@@ -54,12 +67,14 @@ namespace CasteRush.Units
             }
 
             m_currentWaypointIndex++;
-            m_currentTarget = PathWaypoints.Points[m_currentWaypointIndex];
+            SetCurrentTarget( PathWaypoints.Points[m_currentWaypointIndex]);
         }
 
         private void EndPath()
         {
             //TODO Notify End Path
+
+            //Shutdown object
             StopAllCoroutines();
             gameObject.SetActive(false);
         }
